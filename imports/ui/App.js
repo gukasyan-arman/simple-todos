8263de56b53e74b4044cab4ac7456778124a49f2
Arman Gukasyan <gukasian.a.a@gmail.com>
2020-04-24 20:15:45 +0300

    7.5  Update data container to return incompleteCount

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -79,8 +79,10 @@ class App extends Component {
       );
     }
   }
+  
 export default withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 })(App);
